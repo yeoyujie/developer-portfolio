938f21281df1a733cad084a08b8686ce11c2eb0a
yeoyujie <yeo.yujie@gmail.com>
2023-06-19 18:29:06 +0800

    Added navbar elements

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,17 +4,31 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
+import Link from "next/link";
 
+const pages = [
+  { title: "About", href: "/about-me" },
+  { title: "News of the Day", href: "/news" },
+  { title: "Experience", href: "/experience" },
+  { title: "Projects", href: "/projects" },
+  { title: "Games", href: "/games" },
+  { title: "Contact", href: "/contact" },
+];
 
 export default function Navbar() {
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Navbar
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  );
-}
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Navbar
+          </Typography>
+          {pages.map((page) => (
+            <Link key={page.href} href={page.href} passHref>
+              <Button color="inherit">{page.title}</Button>
+            </Link>
+          ))}
+        </Toolbar>
+      </AppBar>
+    );
+  }
