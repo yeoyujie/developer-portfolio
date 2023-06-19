6ef5e171e01d411508d098e7c5ab0f6cb8af666f
yeoyujie <yeo.yujie@gmail.com>
2023-06-19 16:12:48 +0800

    Init nextjs 13 app

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+
+export default function Navbar() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Navbar
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
