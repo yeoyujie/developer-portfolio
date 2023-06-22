dfc6c3aa56856ef143bc2976682d34bab9001611
yeoyujie <yeo.yujie@gmail.com>
2023-06-22 11:05:42 +0800

    Added a nicer looking carousel using react slick

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,7 +8,11 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import Carousel from "react-material-ui-carousel";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './module.css'
+
 
 export default function Contact() {
   const [name, setName] = useState("");
@@ -24,11 +28,26 @@ export default function Contact() {
       email: "charlie@example.com",
       message: "Keep up the good work!",
     },
+    { name: "Delta", email: "delta@example.com", message: "Hello!" },
+    { name: "Echo", email: "echo@example.com", message: "Great website!" },
+    {
+      name: "Foxtrot",
+      email: "foxtrot@example.com",
+      message: "Keep up the good work!",
+    },
   ];
+
+  const settings = {
+    centerMode: true,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 1500,
+  };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
   };
+
   return (
     <>
       <IconButton href="https://www.instagram.com/yu_jieee">
@@ -66,9 +85,9 @@ export default function Contact() {
           </form>
         </CardContent>
       </Card>
-      <Carousel>
+      <Slider {...settings}>
         {messages.map((msg) => (
-          <Card key={msg.email}>
+          <Card key={msg.email} className="square-card">
             <CardContent>
               <h3>{msg.name}</h3>
               <p>{msg.email}</p>
@@ -76,7 +95,7 @@ export default function Contact() {
             </CardContent>
           </Card>
         ))}
-      </Carousel>
+      </Slider>
     </>
   );
 }
