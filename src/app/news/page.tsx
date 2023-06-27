49136198f0147515fcfaba1a133f7941eb92833b
yeoyujie <yeo.yujie@gmail.com>
2023-06-27 18:24:57 +0800

    Added news api

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -2,9 +2,6 @@
 
 import { useState, useEffect } from "react";
 
-const NewsAPI = require("newsapi");
-const newsapi = new NewsAPI(process.env.NEXT_PUBLIC_NEWS_API_KEY);
-
 interface Article {
   title: string;
   description: string;
@@ -12,87 +9,17 @@ interface Article {
   urlToImage: string;
 }
 
-const ListOfStories = () => {
-  const [articles, setArticles] = useState<Article[]>([]);
-
-  useEffect(() => {
-    const fetchArticles = async () => {
-      const response = await newsapi.v2.topHeadlines({
-        country: "us",
-      });
-      setArticles(response.articles);
-    };
-    fetchArticles();
-  }, []);
+export default async function ListOfStories() {
+  const res = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`
+  );
+  const posts = await res.json();
 
   return (
     <div>
-      {articles.map((article) => (
-        <p key={article.url}>{article.title}</p>
-      ))}
+        {posts.articles.map((article:any) => (
+            <p key={article.url}>{article.title}</p>
+        ))}
     </div>
   );
-};
-
-export default ListOfStories;
-
-// const getArticles = async () => {
-//  const res = await()
-//  return res.json();
-// }
-
-// export default async function ListOfStories() {
-//     const posts = await getArticles();
-
-//     return (
-//         <div>
-//             {posts.map((post:any) => {
-//                 return <p>{post.title}</p>
-//             })}
-//         </div>
-//     )
-// }
-
-// interface Article {
-//   title: string;
-//   description: string;
-//   url: string;
-//   urlToImage: string;
-// }
-
-// export default function NewsPage() {
-//   const [articles, setArticles] = useState<Article[]>([]);
-
-//   newsapi.v2.topHeadlines({
-//     country: 'us'
-//   }).then((response: any) => {
-//     console.log(response);
-
-//   });
-
-//   const fetchArticles = async () => {
-//     try {
-//       const response = await newsapi.v2.topHeadlines({
-//         country: "us",
-//       });
-//       setArticles(response.articles);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       {articles.map((article) => (
-//         <div key={article.url} style={{ display: "flex", margin: "1rem" }}>
-//           <img src={article.urlToImage} alt={article.title} width={200} />
-//           <div style={{ marginLeft: "1rem" }}>
-//             <h2>{article.title}</h2>
-//             <p>{article.description}</p>
-//             <a href={article.url}>Read more</a>
-//           </div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
+}
