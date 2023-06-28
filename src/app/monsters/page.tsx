a87e2bb49ad8ee94956e573c6c2973d2db36f1e4
yeoyujie <yeo.yujie@gmail.com>
2023-06-28 09:49:12 +0800

    Added card and tab view in news

---
+++ src/app/monsters/page.tsx
@@ -0,0 +1,3 @@
+export default function Monsters() {
+  return <div></div>;
+}
--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,6 +1,20 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardContent,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 
 interface NewsApiResponse {
   status: string;
@@ -24,28 +38,68 @@ interface Source {
   name: string;
 }
 
-const ListOfStories = () => {
-  const [articles, setArticles] = useState<Article[]>([]);
+type ViewMode = "card" | "tab";
 
+export default function News() {
+  const [articles, setArticles] = useState<Article[]>([]);
+  const [viewMode, setViewMode] = useState<ViewMode>("card");
+  const [numArticles, setNumArticles] = useState(5);
+
+  // Fetch top headlines from News API
   useEffect(() => {
     const fetchArticles = async () => {
       const res = await fetch(
         `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`
       );
-      const data = await res.json();
-      console.log(data);
-      setArticles(data.articles);
+      const NewsApiResponse = await res.json();
+      setArticles(NewsApiResponse.articles);
     };
     fetchArticles();
   }, []);
 
   return (
     <div>
-      {articles.map((article) => (
-        <p key={article.url}>{article.title}</p>
-      ))}
+      <ButtonGroup variant="contained">
+        <Button onClick={() => setViewMode("card")}>Card View</Button>
+        <Button onClick={() => setViewMode("tab")}>Tab View</Button>
+      </ButtonGroup>
+      <ButtonGroup variant="contained">
+        <Button onClick={() => setNumArticles(5)}>5</Button>
+        <Button onClick={() => setNumArticles(10)}>10</Button>
+        <Button onClick={() => setNumArticles(25)}>25</Button>
+      </ButtonGroup>
+      {viewMode === "card" ? (
+        <Grid container spacing={2}>
+          {articles.map((article) => (
+            <Grid item xs={12} sm={6} md={4} key={article.url}>
+              <Card>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    {article.title}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      ) : (
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Title</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {articles.map((article) => (
+                <TableRow key={article.url}>
+                  <TableCell>{article.title}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
     </div>
   );
-};
-
-export default ListOfStories;
+}
