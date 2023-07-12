f74da9b9afd91f0886d6c5e5d27aa895757dbb59
yeoyujie <yeo.yujie@gmail.com>
2023-07-12 16:02:22 +0800

    Chnage home icon to own logo

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import * as React from "react";
+import { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "@/assets/icons/logo.png";
+import LogoNeon from "@/assets/icons/logo neon.png";
 
 const pages = [
   { title: "About", href: "/about-me" },
@@ -13,11 +17,13 @@ const pages = [
   { title: "Experience", href: "/experience" },
   { title: "Projects", href: "/projects" },
   { title: "Games", href: "/games" },
-  {title: "Monsters", href: "/monsters"},
+  { title: "Monsters", href: "/monsters" },
   { title: "Contact", href: "/contact" },
 ];
 
 export default function Navbar() {
+  const [logoSrc, setLogoSrc] = useState(Logo);
+
   return (
     <AppBar
       position="static"
@@ -29,7 +35,14 @@ export default function Navbar() {
     >
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <Link href="/">Home</Link>
+          <Link
+            href="/"
+            passHref
+            onMouseEnter={() => setLogoSrc(LogoNeon)}
+            onMouseLeave={() => setLogoSrc(Logo)}
+          >
+            <Image src={logoSrc} alt="Home" width={60} height={60} />
+          </Link>
         </Typography>
         {pages.map((page) => (
           <Link key={page.href} href={page.href} passHref>
