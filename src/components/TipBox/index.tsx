1480a8d3c0b8e8fb5caca4058d543f50b3ab6197
yeoyujie <yeo.yujie@gmail.com>
2023-07-17 12:16:31 +0800

    Change tip box to open a new window instead

--- src/components/TipBox/index.tsx
+++ src/components/TipBox/index.tsx
@@ -1,3 +1,5 @@
+import "./component.css";
+
 interface TipBoxProps {
   message: string;
   onClose: () => void;
@@ -5,9 +7,14 @@ interface TipBoxProps {
 
 export default function TipBox({ message, onClose }: TipBoxProps) {
   return (
-    <div className="tip-box">
-      <p>{message}</p>
-      <button onClick={onClose}>Close</button>
+    <div className="modal">
+      <div className="modal-content">
+        <div className="tip-box">
+          <p>{message}</p>
+          <button onClick={onClose}>Close</button>
+        </div>
+      </div>
     </div>
   );
 }
+
