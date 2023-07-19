5997c569333a4e65bf7b7d74f9f265bdd15247d0
yeoyujie <yeo.yujie@gmail.com>
2023-07-19 15:38:00 +0800

    Fix score-box element resizing issue when large-score is animated

--- src/components/ScoreDisplay/index.tsx
+++ src/components/ScoreDisplay/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import "./component.css";
 
 interface ScoreDisplayProps {
@@ -7,16 +8,36 @@ interface ScoreDisplayProps {
 
 const ScoreDisplay: React.FC<ScoreDisplayProps> = ({ timeLeft, score }) => {
   const timeLeftClass = timeLeft <= 10 ? "time-left-danger" : "";
+
+  const [animate, setAnimate] = useState(true);
+  const [prevScore, setPrevScore] = useState(0);
+
+  useEffect(() => {
+    setAnimate(true);
+    const timeout = setTimeout(() => {
+      setAnimate(false);
+      setPrevScore(score);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, [score]);
+
+  const scoreDiff = score - prevScore;
+
   return (
     <>
       <div className="score-container">
         <div className="score-box">
           <span>Time left</span>
-          <span className={timeLeftClass}>{timeLeft}</span>
+          <span className={`time-left ${timeLeftClass}`}>{timeLeft}</span>
         </div>
         <div className="score-box">
           <span>Score</span>
-          <span>{score}</span>
+          <span className="score">{score}</span>
+          {animate && (
+            <span className={`large-score ${animate ? "animate" : ""}`}>
+              {scoreDiff > 0 ? `+${scoreDiff}` : `${scoreDiff}`}
+            </span>
+          )}
         </div>
       </div>
     </>
